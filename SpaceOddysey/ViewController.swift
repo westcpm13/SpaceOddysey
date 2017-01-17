5c1fe38301946e739e0e90a27f79d29be9264fb5
Paweł Trojan <ptrojan@sointeractive.pl>
2017-01-17 20:46:06 +0100

    initial project commit

---
+++ SpaceOddysey/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SpaceOddysey
+//
+//  Created by ptrojan on 17.01.2017.
+//  Copyright © 2017 ptrojan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
