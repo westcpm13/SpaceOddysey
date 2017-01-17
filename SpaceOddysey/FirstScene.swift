3f4355d143aed7e5e3c440c6b128499a1b0970d4
Paweł Trojan <westcpm13@wp.pl>
2017-01-17 21:31:12 +0100

    First Scene

---
+++ SpaceOddysey/FirstScene.swift
@@ -0,0 +1,20 @@
+//
+//  FirstScene.swift
+//  SpaceOddysey
+//
+//  Created by ptrojan on 17.01.2017.
+//  Copyright © 2017 ptrojan. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+
+class FirstScene : SKScene {
+    
+    override func didMove(to view: SKView) {
+        view.backgroundColor = UIColor.red
+    }
+    
+}
+
--- SpaceOddysey/ViewController.swift
+++ SpaceOddysey/ViewController.swift
@@ -7,12 +7,20 @@
 //
 
 import UIKit
+import SpriteKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let firstScene = FirstScene(size: self.view.bounds.size)
+        let skView = self.view as! SKView
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+        skView.ignoresSiblingOrder = true
+        skView.presentScene(firstScene)
     }
 
     override func didReceiveMemoryWarning() {
